0937cd67ea86f055c0dc1f6f43e0d891c2b8f920
Mekaret Eriker <mekaret@revhosts.net>
2015-08-15 21:12:09 +0200

    bind to Eve Central

--- lib/reccore.rb
+++ lib/reccore.rb
@@ -6,6 +6,142 @@ require 'reccore/vash'
 require 'reccore/marketstat'
 
 module Reccore
-  def self.min_self(_str)
+  class << self; attr_accessor :cache end
+  @cache = Vash.new
+
+  # :volume => element.css('buy/volume').text,
+  # :avg => element.css('buy/avg').text,
+  # :max => element.css('buy/max').text,
+  # :min => element.css('buy/min').text,
+  # :stddev => element.css('buy/stddev').text,
+  # :median => element.css('buy/median').text,
+  # :percentile => element.css('buy/percentile').text
+
+  class EC
+    def get_buy_volume(typeid)
+      if Reccore.cache[typeid.to_s].nil?
+        Reccore::Marketstat.new(:typeid => typeid.to_s)
+        Reccore.cache[typeid.to_s].first[:buy][:volume]
+      else
+        Reccore.cache[typeid.to_s].first[:buy][:volume]
+      end
+    end
+
+    def get_buy_avg(typeid)
+      if Reccore.cache[typeid.to_s].nil?
+        Reccore::Marketstat.new(:typeid => typeid.to_s)
+        Reccore.cache[typeid.to_s].first[:buy][:avg]
+      else
+        Reccore.cache[typeid.to_s].first[:buy][:avg]
+      end
+    end
+
+    def get_buy_max(typeid)
+      if Reccore.cache[typeid.to_s].nil?
+        Reccore::Marketstat.new(:typeid => typeid.to_s)
+        Reccore.cache[typeid.to_s].first[:buy][:max]
+      else
+        Reccore.cache[typeid.to_s].first[:buy][:max]
+      end
+    end
+
+    def get_buy_min(typeid)
+      if Reccore.cache[typeid.to_s].nil?
+        Reccore::Marketstat.new(:typeid => typeid.to_s)
+        Reccore.cache[typeid.to_s].first[:buy][:min]
+      else
+        Reccore.cache[typeid.to_s].first[:buy][:min]
+      end
+    end
+
+    def get_buy_stddev(typeid)
+      if Reccore.cache[typeid.to_s].nil?
+        Reccore::Marketstat.new(:typeid => typeid.to_s)
+        Reccore.cache[typeid.to_s].first[:buy][:stddev]
+      else
+        Reccore.cache[typeid.to_s].first[:buy][:stddev]
+      end
+    end
+
+    def get_buy_median(typeid)
+      if Reccore.cache[typeid.to_s].nil?
+        Reccore::Marketstat.new(:typeid => typeid.to_s)
+        Reccore.cache[typeid.to_s].first[:buy][:median]
+      else
+        Reccore.cache[typeid.to_s].first[:buy][:median]
+      end
+    end
+
+    def get_buy_percentile(typeid)
+      if Reccore.cache[typeid.to_s].nil?
+        Reccore::Marketstat.new(:typeid => typeid.to_s)
+        Reccore.cache[typeid.to_s].first[:buy][:percentile]
+      else
+        Reccore.cache[typeid.to_s].first[:buy][:percentile]
+      end
+    end
+
+    def get_sell_volume(typeid)
+      if Reccore.cache[typeid.to_s].nil?
+        Reccore::Marketstat.new(:typeid => typeid.to_s)
+        Reccore.cache[typeid.to_s].last[:sell][:volume]
+      else
+        Reccore.cache[typeid.to_s].last[:sell][:volume]
+      end
+    end
+
+    def get_sell_avg(typeid)
+      if Reccore.cache[typeid.to_s].nil?
+        Reccore::Marketstat.new(:typeid => typeid.to_s)
+        Reccore.cache[typeid.to_s].last[:sell][:avg]
+      else
+        Reccore.cache[typeid.to_s].last[:sell][:avg]
+      end
+    end
+
+    def get_sell_max(typeid)
+      if Reccore.cache[typeid.to_s].nil?
+        Reccore::Marketstat.new(:typeid => typeid.to_s)
+        Reccore.cache[typeid.to_s].last[:sell][:max]
+      else
+        Reccore.cache[typeid.to_s].last[:sell][:max]
+      end
+    end
+
+    def get_sell_min(typeid)
+      if Reccore.cache[typeid.to_s].nil?
+        Reccore::Marketstat.new(:typeid => typeid.to_s)
+        Reccore.cache[typeid.to_s].last[:sell][:min]
+      else
+        Reccore.cache[typeid.to_s].last[:sell][:min]
+      end
+    end
+
+    def get_sell_stddev(typeid)
+      if Reccore.cache[typeid.to_s].nil?
+        Reccore::Marketstat.new(:typeid => typeid.to_s)
+        Reccore.cache[typeid.to_s].last[:sell][:stddev]
+      else
+        Reccore.cache[typeid.to_s].last[:sell][:stddev]
+      end
+    end
+
+    def get_sell_median(typeid)
+      if Reccore.cache[typeid.to_s].nil?
+        Reccore::Marketstat.new(:typeid => typeid.to_s)
+        Reccore.cache[typeid.to_s].last[:sell][:median]
+      else
+        Reccore.cache[typeid.to_s].last[:sell][:median]
+      end
+    end
+
+    def get_sell_percentile(typeid)
+      if Reccore.cache[typeid.to_s].nil?
+        Reccore::Marketstat.new(:typeid => typeid.to_s)
+        Reccore.cache[typeid.to_s].last[:sell][:percentile]
+      else
+        Reccore.cache[typeid.to_s].last[:sell][:percentile]
+      end
+    end
   end
 end
--- lib/reccore/marketstat.rb
+++ lib/reccore/marketstat.rb
@@ -13,20 +13,22 @@ module Reccore
     # regionlimit : Restrict statistics to a region. Can be specified more than once.
     # usesystem : Restrict statistics to a system.
     def initialize(options = {})
-      # hours = 24, typeid, minQ = nil, regionlimit = nil, usesystem = nil
+      # hours = 24, typeid, minQ = nil, regionlimit = nil, usesystem = '30000142' (Jita)
       @args = ''
-      @args += "#{options[:hours]}&" unless options[:hours].nil?
+      @args += "hours=#{options[:hours]}&" unless options[:hours].nil?
       parse_typeid(options[:typeid])
-      @args += "#{options[:minQ]}&" unless options[:minQ].nil?
+      @args += "minQ=#{options[:minQ]}&" unless options[:minQ].nil?
       parse_regionlimit(options[:regionlimit]) unless options[:regionlimit].nil?
-      @args += "#{options[:usesystem]}&" unless options[:usesystem].nil?
-      @marketData = Nokogiri::XML(open(Marketstat.endpoint + @args.chomp('&')))
-      price = @marketData.css("all avg")[0].content
-      puts "The average market price for type #{options[:typeid]} is #{price}"
+      @args += 'usesystem=30000142&' if options[:regionlimit].nil?
+      @args += "usesystem=#{options[:usesystem]}&" unless options[:usesystem].nil?
+      p @args.chomp('&')
+      xml = Nokogiri::XML(open(Marketstat.endpoint + @args.chomp('&')))
+      update(xml)
+      Reccore.cache.cleanup!
     end
 
     def parse_typeid(typeid)
-      if typeid.class == Array
+      if typeid.is_a?(Array)
         typeid.each { |type| @args += "typeid=#{type}&" }
       else
         @args += "typeid=#{typeid}&"
@@ -34,15 +36,44 @@ module Reccore
     end
 
     def parse_regionlimit(regionlimit)
-      if regionlimit.class == Array
+      if regionlimit.is_a?(Array)
         regionlimit.each { |region| @args += "regionlimit=#{region}&" }
       else
         @args += "regionlimit=#{regionlimit}&"
       end
     end
 
-    def test_xml
-      @marketData
-    end  
+    def update(xml)
+      elements = xml.xpath('//type')
+      elements.each do |element|
+        Reccore.cache[element['id'], 3600] = update_buy(element), update_sell(element)
+      end
+    end
+
+    def update_buy(element)
+      {:buy =>
+        {:volume => element.css('buy/volume').text,
+         :avg => element.css('buy/avg').text,
+         :max => element.css('buy/max').text,
+         :min => element.css('buy/min').text,
+         :stddev => element.css('buy/stddev').text,
+         :median => element.css('buy/median').text,
+         :percentile => element.css('buy/percentile').text,
+          },
+        }
+    end
+
+    def update_sell(element)
+      {:sell =>
+        {:volume => element.css('sell/volume').text,
+         :avg => element.css('sell/avg').text,
+         :max => element.css('sell/max').text,
+         :min => element.css('sell/min').text,
+         :stddev => element.css('sell/stddev').text,
+         :median => element.css('sell/median').text,
+         :percentile => element.css('sell/percentile').text,
+          },
+        }
+    end
   end
 end
